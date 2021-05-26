b17123a7f365c3fc677760b11dd3991a2701f023
Abanoub Shenouda <Abanoub Shenouda>
2021-05-25 21:28:24 -0400

    Added assertArrays Equal.js code

---
+++ assertArraysEqual.js
@@ -0,0 +1,47 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion failed ${actual} != ${expected}`);
+  }
+};
+
+const eqArrays = (array1, array2) => {
+  let match = true;
+  for (let index = 0; index < array1.length; index++) {
+    if (array1[index] !== array2[index]) {
+      match = false;
+    }
+  }
+  return match;
+};
+
+const assertArraysEqual = (array1, array2) => {
+  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+  assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should not PASS
+  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+};
+
+// eqArrays([1, 2, 3], [1, 2, 3]); // => true
+// eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should not PASS
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+// eqArrays([1, 2, 3], [1, 2, 3]); // => true
+// eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should not PASS
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertArraysEqual();
